51021d0d6eb791e51e19b149ea7668a8a9e844ac
MANUEL SANZ ROMERO <manuel.sanz@vilt-group.com>
2025-09-25 10:30:01 +0200

    Pruebas imagenes

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,48 +1,19 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-tab-bar color="light">
-      <ion-tab-button tab="1">
-        <ion-icon :icon="heart" />
-        <ion-label>Favorites</ion-label>
-        <ion-badge color="danger"></ion-badge>
-      </ion-tab-button>
-
-      <ion-tab-button tab="2">
-        <ion-icon :icon="musicalNote" />
-        <ion-label>Music</ion-label>
-      </ion-tab-button>
-
-      <ion-tab-button tab="3">
-        <ion-icon :icon="calendar" />
-        <ion-label>Calendar</ion-label>
-        <ion-badge color="danger">47</ion-badge>
-      </ion-tab-button>
-    </ion-tab-bar>
-  </ion-page>
-
+  <ion-card v-for="(ruta, nombre) in productos" :key="nombre">
+    <img :alt="nombre" :src="ruta" />
+    <ion-card-header>
+      <ion-card-title>{{ nombre }}</ion-card-title>
+    </ion-card-header>
+  </ion-card>
 </template>
 
-<script lang="ts">
-import { IonBadge, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/vue';
-import { heart, calendar, musicalNote } from 'ionicons/icons';
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue'
 
-export default defineComponent({
-  components: {
-    IonBadge,
-    IonTabBar,
-    IonTabButton,
-    IonIcon,
-    IonLabel,
-  },
-  setup() {
-    return { heart, calendar, musicalNote };
-  },
-});
+// objeto de productos con nombre → ruta
+const productos: Record<string, string> = {
+  platano: '../assets/img/products/platano.png',
+  naranja: '/img/products/naranja.png',
+  leche: '/img/products/leche.png'
+}
 </script>
