ce7203e50bee2d5b414add366a0adcb944d9d305
MANUEL SANZ ROMERO <manuel.sanz@vilt-group.com>
2025-12-12 10:31:55 +0100

    Correccion borrar notas

--- src/views/InventoryPage.vue
+++ src/views/InventoryPage.vue
@@ -703,7 +703,7 @@ async function getPantryItems(pantryCode: string) {
 async function togglePurchaseState(item: Item) {
   try {
     const ref = doc(db, 'items', item.id)
-    await updateDoc(ref, { inPurchase: !item.inPurchase })
+    await updateDoc(ref, { inPurchase: !item.inPurchase, notePurchase: null })
   } catch (err) {
     console.error('Error al actualizar inPurchase:', err)
     await showToast(`No se pudo actualizar el estado de ${item.name}.`, 'danger')
